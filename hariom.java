fbb3778b25e24bf6b30edbae2dc6b5eebff30da8
Hariomsingh11 <92686828+Hariomsingh11@users.noreply.github.com>
2022-10-18 21:50:53 +0530

    Create hariom.java

---
+++ hariom.java
@@ -0,0 +1,13 @@
+public class factorialusingforloop {
+    public static void main(String[] args)
+    { int n=5;
+       // int i=1;
+        int factorial=1;
+        for(int i=1;i<=n;i++)
+        {
+            factorial =factorial*i;
+        }
+        System.out.println(factorial);
+
+}
+}
